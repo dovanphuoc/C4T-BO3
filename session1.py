07991683cf403e5b778b02200c1bf37891a190eb
thundergod3 <congthundergodphan@gmail.com>
2019-06-09 12:01:35 +0700

    first commit

---
+++ session1.py
@@ -0,0 +1,13 @@
+# print("Hello World everyone !!!")
+# Input = input("nhap cai gi vao")
+# number = "1"
+# print(int(number))
+
+# chieuCao = float(input("Nhap chieu cao: "))
+# canhDay = float(input("Nhap canh day: "))
+# dienTamGiac = (chieuCao * canhDay) * 0.5
+# print(dienTamGiac)
+
+banKinh = float(input("Nhap ban kinh: "))
+dienTichHinhTron = banKinh*3.14
+print("Dien tich hinh tron = ",dienTichHinhTron)
